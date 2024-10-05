6f14e92d81d9fcf4875f84c129793eaa75f6e621
hjoo830 <hjoo830@naver.com>
2024-10-06 00:34:31 +0900

    ✨ Feat: Add ProfileSection to App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./App.css";
 
 import StartSection from "./components/StartSection.js";
+import ProfileSection from "./components/ProfileSection.js";
+
 function App() {
   return (
     <div className="App">
       <StartSection />
+      <ProfileSection />
     </div>
   );
 }
