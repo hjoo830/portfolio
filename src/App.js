3c9ff7e4a4582471178ffd06a49f38d8ab5905a0
hjoo830 <hjoo830@naver.com>
2024-10-07 00:18:23 +0900

    ✨ Feat: Add ExperienceSection to App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 import StartSection from "./components/StartSection.js";
 import ProfileSection from "./components/ProfileSection.js";
+import ExperienceSection from "./components/ExperienceSection.js";
 
 function App() {
   return (
     <div className="App">
       <StartSection />
       <ProfileSection />
+      <ExperienceSection />
     </div>
   );
 }
