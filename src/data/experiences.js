929c43ba04277782b207fa96d3968a00de1af350
hjoo830 <hjoo830@naver.com>
2024-10-10 00:46:02 +0900

    ✨ Feat: Update experiences data structure in experiences.js

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,11 +1,41 @@
+import swEduVolunteerImage from "../assets/sw교육봉사.jpeg";
+import informationProcessingEngineer from "../assets/정보처리기사.jpg";
+
 export const experiences = [
-  { title: "SW 교육 봉사", date: "2022.07.25 ~ 2022.07.29" },
-  { title: "정보처리기사 취득", date: "2024.06.18" },
-  { title: "네이버 부스트캠프 베이직", date: "2024.06.24 ~ 2024.07.05" },
+  {
+    title: "SW 교육 봉사",
+    date: "2022.07.25 ~ 2022.07.29",
+    type: "file",
+    imgSrc: swEduVolunteerImage,
+  },
+  {
+    title: "정보처리기사 취득",
+    date: "2024.06.18",
+    type: "file",
+    imgSrc: informationProcessingEngineer,
+  },
+  {
+    title: "네이버 부스트캠프 베이직",
+    date: "2024.06.24 ~ 2024.07.05",
+    type: "github",
+    link: "private",
+  },
   {
     title: "구름톤 딥 다이브 프론트엔드 1기",
     date: "2024.08.19 ~ 2025.03.28",
+    type: "github",
+    link: "https://github.com/hjoo830/9oormthon-DEEP-DIVE",
   },
-  { title: "우아한테크코스 프리코스", date: "2024.10.15 ~ 2024.11.11" },
-  { title: "영남대학교 컴퓨터공학과 졸업", date: "2025.02.21" },
+  {
+    title: "우아한테크코스 프리코스",
+    date: "2024.10.15 ~ 2024.11.11",
+    type: "github",
+    link: "private",
+  },
+  {
+    title: "영남대학교 컴퓨터공학과 졸업",
+    date: "2025.02.21",
+    type: "file",
+    imgSrc: "",
+  },
 ];
