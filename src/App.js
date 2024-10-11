8ca7c0f69049cffea4bfbc6bf89782f4860991bf
hjoo830 <hjoo830@naver.com>
2024-10-11 23:00:25 +0900

    ✨ Feat: Add TechSection to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import StartSection from "./components/StartSection.js";
 import ProfileSection from "./components/ProfileSection.js";
 import ExperienceSection from "./components/ExperienceSection.js";
+import TechStackSection from "./components/TechStackSection.js";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <StartSection />
       <ProfileSection />
       <ExperienceSection />
+      <TechStackSection />
     </div>
   );
 }
