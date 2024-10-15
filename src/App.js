a4a6cc749961334c889ba60cb730b24153f630f0
hjoo830 <hjoo830@naver.com>
2024-10-15 20:24:29 +0900

    ✨ Feat: Add Footer to App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ProfileSection from "./components/ProfileSection.js";
 import ExperienceSection from "./components/ExperienceSection.js";
 import TechStackSection from "./components/TechStackSection.js";
 import ProjectSection from "./components/ProjectSection.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <ExperienceSection />
       <TechStackSection />
       <ProjectSection />
+      <Footer />
     </div>
   );
 }
